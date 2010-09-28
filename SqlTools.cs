a1e51ce1855c8ab068349811659b99854ef20f80
weiwong <weiwong@localhost>
2010-09-28 17:29:35 -0400

    Added SqlTools.GetXXX(DbConnection, string) methods, allowing easy retrieval of scalar data by providing a SQL statement and a DbConnection alone

--- SqlTools.cs
+++ SqlTools.cs
@@ -21,7 +21,7 @@ namespace SharpNick
 			return reader.GetString(index);
 		}
 		/// <summary>
-		/// Gets the Decimal value for the scalar value returned by the DbCommand instance.
+		/// Gets the string value for the scalar value returned by the DbCommand instance.
 		/// </summary>
 		/// <param name="cmd"></param>
 		/// <returns></returns>
@@ -30,6 +30,18 @@ namespace SharpNick
 			var result = cmd.ExecuteScalar();
 			if (result is DBNull || result == null) return string.Empty;
 			return Convert.ToString(result);
+		}
+		/// <summary>
+		/// Gets the string value for the scalar value returned by an SQL query, running against an instance of a database connection.
+		/// </summary>
+		/// <param name="connection"></param>
+		/// <param name="query"></param>
+		/// <returns></returns>
+		public static string GetString(DbConnection connection, string query)
+		{
+			var cmd = connection.CreateCommand();
+			cmd.CommandText = query;
+			return GetString(cmd);
 		}
 		/// <summary>
 		/// Gets the Int32 value at the specified index of a data reader. If DBNull, returns 0.
@@ -54,6 +66,18 @@ namespace SharpNick
 			return Convert.ToInt32(result);
 		}
 		/// <summary>
+		/// Gets the Int32 value for the scalar value returned by an SQL query, running against an instance of a database connection.
+		/// </summary>
+		/// <param name="connection"></param>
+		/// <param name="query"></param>
+		/// <returns></returns>
+		public static int GetInt32(DbConnection connection, string query)
+		{
+			var cmd = connection.CreateCommand();
+			cmd.CommandText = query;
+			return GetInt32(cmd);
+		}
+		/// <summary>
 		/// Gets the date/time value at the specified index of a data reader. If DBNull, returns DateTime.MinValue.
 		/// </summary>
 		/// <param name="reader"></param>
@@ -65,7 +89,7 @@ namespace SharpNick
 			return reader.GetDateTime(index);
 		}
 		/// <summary>
-		/// Gets the Decimal value for the scalar value returned by the DbCommand instance.
+		/// Gets the date/time value for the scalar value returned by the DbCommand instance.
 		/// </summary>
 		/// <param name="cmd"></param>
 		/// <returns></returns>
@@ -74,6 +98,18 @@ namespace SharpNick
 			var result = cmd.ExecuteScalar();
 			if (result is DBNull || result == null) return DateTime.MinValue;
 			return Convert.ToDateTime(result);
+		}
+		/// <summary>
+		/// Gets the date/time value for the scalar value returned by an SQL query, running against an instance of a database connection.
+		/// </summary>
+		/// <param name="connection"></param>
+		/// <param name="query"></param>
+		/// <returns></returns>
+		public static DateTime GetDateTime(DbConnection connection, string query)
+		{
+			var cmd = connection.CreateCommand();
+			cmd.CommandText = query;
+			return GetDateTime(cmd);
 		}
 		/// <summary>
 		/// Gets the boolean value at the specified index of a data reader. If DBNull, returns false.
@@ -87,7 +123,7 @@ namespace SharpNick
 			return reader.GetBoolean(index);
 		}
 		/// <summary>
-		/// Gets the Decimal value for the scalar value returned by the DbCommand instance.
+		/// Gets the boolean value for the scalar value returned by the DbCommand instance.
 		/// </summary>
 		/// <param name="cmd"></param>
 		/// <returns></returns>
@@ -96,6 +132,18 @@ namespace SharpNick
 			var result = cmd.ExecuteScalar();
 			if (result is DBNull || result == null) return false;
 			return Convert.ToBoolean(result);
+		}
+		/// <summary>
+		/// Gets the boolean value for the scalar value returned by an SQL query, running against an instance of a database connection.
+		/// </summary>
+		/// <param name="connection"></param>
+		/// <param name="query"></param>
+		/// <returns></returns>
+		public static bool GetBoolean(DbConnection connection, string query)
+		{
+			var cmd = connection.CreateCommand();
+			cmd.CommandText = query;
+			return GetBoolean(cmd);
 		}
 		/// <summary>
 		/// Gets the Decimal value at the specified index of a data reader. If DBNull, returns 0.
@@ -118,6 +166,18 @@ namespace SharpNick
 			var result = cmd.ExecuteScalar();
 			if (result is DBNull || result == null) return 0;
 			return Convert.ToDecimal(result);
+		}
+		/// <summary>
+		/// Gets the Decimal value for the scalar value returned by an SQL query, running against an instance of a database connection.
+		/// </summary>
+		/// <param name="connection"></param>
+		/// <param name="query"></param>
+		/// <returns></returns>
+		public static decimal GetDecimal(DbConnection connection, string query)
+		{
+			var cmd = connection.CreateCommand();
+			cmd.CommandText = query;
+			return GetDecimal(cmd);
 		}
 		/// <summary>
 		/// Executes a short query, not expecting any results from the query.
