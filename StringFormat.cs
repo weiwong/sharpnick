76ee319b32481e64bde95698141039d8e63016e2
weiwong <weiwong@localhost>
2011-05-05 12:50:43 -0400

    Updated email regex to a more robust version copied from Postmark API project

--- StringFormat.cs
+++ StringFormat.cs
@@ -26,7 +26,7 @@ namespace SharpNick
 		/// <summary>
 		/// Regex instance that determines if a string is a valid email.
 		/// </summary>
-		private static readonly Regex EmailRegex = new Regex(@"^\s*[\w\d\.!#$%&'*+-/=\?^_`\{|\}\~]+@[\w\d-\.]+\.\w+\s*$", RegexOptions.Compiled);
+		private static readonly Regex EmailRegex = new Regex(@"^[-_a-z0-9'+*$^&%=~!?{}]+(?:\.[-_a-z0-9'+*$^&%=~!?{}]+)*@(?:(?![-.])[-a-z0-9.]+(?<![-.])\.[a-z]{2,6}|\d{1,3}(?:\.\d{1,3}){3})(?::\d+)?$", RegexOptions.Compiled);
 		/// <summary>
 		/// Regex instance that finds string sequence of two or more space characters.
 		/// </summary>
